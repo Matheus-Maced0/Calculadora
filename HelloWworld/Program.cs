b32f2bcfcafa808900322850e2d8e8f5be5871ff
Matheus-Maced0 <matheusalmeidademacedo@gmail.com>
2022-10-15 06:10:55 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWworld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE QUAL OPERAÇÃO VOCÊ DESEJA REALIZAR:\n1- Soma\n2- Subtração\n3_ Multiplicação\n
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
